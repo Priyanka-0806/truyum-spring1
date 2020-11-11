5e03293c0d16b8eda6a1a85d55b1f50f1a1b47a2
Priyanka-0806 <73281086+Priyanka-0806@users.noreply.github.com>
2020-11-11 13:40:19 +0530

    Case Study updated

---
+++ src/main/java/com/cognizant/truyum/dao/CartEmptyException.java
@@ -0,0 +1,15 @@
+package com.cognizant.truyum.dao;
+
+@SuppressWarnings("serial")
+public class CartEmptyException extends Exception {
+	// private String message;
+
+	public CartEmptyException() {
+		super();
+	}
+
+	// @Override
+	// public String getMessage() {
+	// return message;
+	// }
+}
---
+++ src/main/java/com/cognizant/truyum/dao/MenuItemDaoCollectionImpl.java
@@ -0,0 +1,75 @@
+package com.cognizant.truyum.dao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.cognizant.truyum.model.MenuItem;
+import com.cognizant.truyum.util.DateUtil;
+
+public class MenuItemDaoCollectionImpl implements MenuItemDao {
+
+	private List<MenuItem> menuItemList;
+
+	public MenuItemDaoCollectionImpl(List<MenuItem> menuItemList) {
+
+		super();
+		this.menuItemList = menuItemList;
+	}
+
+	public List<MenuItem> getMenuItemList() {
+
+		return menuItemList;
+	}
+
+	public void setMenuItemList(List<MenuItem> menuItemList) {
+
+		this.menuItemList = menuItemList;
+	}
+
+	public List<MenuItem> getMenuItemListAdmin() {
+
+		return menuItemList;
+	}
+
+	public List<MenuItem> getMenuItemListCustomer() {
+		List<MenuItem> customerItemList = new ArrayList<>();
+		Date currDate = new DateUtil().convertToDate("20/10/2020");
+
+		for (MenuItem item : menuItemList) {
+			if (item.isActive() && currDate.after(item.getDateOfLaunch())) {
+				customerItemList.add(item);
+			}
+		}
+		return customerItemList;
+	}
+
+	public void modifyMenuItem(MenuItem menuItem) {
+
+		for (MenuItem eachItem : menuItemList) {
+			if (menuItem.equals(eachItem)) {
+				eachItem.setId(menuItem.getId());
+				eachItem.setName(menuItem.getName());
+				eachItem.setPrice(menuItem.getPrice());
+				eachItem.setActive(menuItem.isActive());
+				eachItem.setDateOfLaunch(menuItem.getDateOfLaunch());
+				eachItem.setCategory(menuItem.getCategory());
+				eachItem.setFreeDelivery(menuItem.isFreeDelivery());
+//				eachItem = menuItem;
+				return;
+			}
+		}
+		menuItemList.add(menuItem);
+	}
+
+	public MenuItem getMenuItem(long menuItemId) {
+
+		for (MenuItem menuItem : menuItemList) {
+			if (menuItem.getId() == menuItemId) {
+				return menuItem;
+			}
+		}
+		return null;
+	}
+
+}
